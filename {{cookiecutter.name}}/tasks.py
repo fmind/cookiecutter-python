aaa25f58ae75c21cbf2c31ca634e18d72ebfb5dc
Médéric Hurier <fmind@fmind.me>
2019-11-16 13:59:10 +0100

    Update tasks.py

--- {{cookiecutter.name}}/tasks.py
+++ {{cookiecutter.name}}/tasks.py
@@ -9,7 +9,8 @@ from pathlib import Path
 def venv(c, force=False):
     """Create a virtual environment."""
     if not Path("venv").exists() or force:
-        c.run("python3 -m venv venv --clear")    
+        c.run("python3 -m venv venv --clear") 
+    c.run("venv/bin/pip install -e .")
     c.run("venv/bin/pip install -r requirements.txt")
 
 
