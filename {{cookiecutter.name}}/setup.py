6a392ae04fff3a53a89af30d2959d34e65e0c4a2
Médéric Hurier <fmind@fmind.me>
2019-11-21 15:36:04 +0100

    Update setup.py

--- {{cookiecutter.name}}/setup.py
+++ {{cookiecutter.name}}/setup.py
@@ -6,21 +6,21 @@ import setuptools  # type: ignore
 
 
 META = dict(
-   name="{{cookiecutter.name}}",
-   version="{{cookiecutter.version}}",
-   description="{{cookiecutter.description}}",
-   long_description=open("README.md", "r").read(),
-   long_description_content_type="text/markdown",
-   url="{{cookiecutter.url}}",
-   author="{{cookiecutter.author}}",
-   author_email="{{cookiecutter.email}}",
-   license="{{cookiecutter.license}}",
-   packages=["{{cookiecutter.name}}"],
-   keywords="{{cookiecutter.keywords}}",
-   classifiers=["Development Status :: {{cookiecutter.status}}"],
-   entry_points={"console_scripts": ["{{cookiecutter.name}}={{cookiecutter.name}}.__main__:main"]},
-   python_requires=">={{cookiecutter.python}}",
-   install_requires=[],
+    name="{{cookiecutter.name}}",
+    version="{{cookiecutter.version}}",
+    description="{{cookiecutter.description}}",
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
+    url="{{cookiecutter.url}}",
+    author="{{cookiecutter.author}}",
+    author_email="{{cookiecutter.email}}",
+    license="{{cookiecutter.license}}",
+    packages=["{{cookiecutter.name}}"],
+    keywords="{{cookiecutter.keywords}}",
+    classifiers=["Development Status :: {{cookiecutter.status}}"],
+    entry_points={"console_scripts": ["{{cookiecutter.name}}={{cookiecutter.name}}.__main__:main"]},
+    python_requires=">={{cookiecutter.python}}",
+    install_requires=[],
 )
 
 if __name__ == "__main__":
