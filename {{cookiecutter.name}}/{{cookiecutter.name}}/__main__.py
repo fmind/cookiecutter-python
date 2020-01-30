1f9ad961cb06f6a39b2d5d486a691854edcb1fa6
Médéric Hurier <fmind@fmind.me>
2020-01-30 21:53:49 +0100

    format

--- {{cookiecutter.name}}/{{cookiecutter.name}}/__main__.py
+++ {{cookiecutter.name}}/{{cookiecutter.name}}/__main__.py
@@ -6,10 +6,12 @@ import argparse
 
 PARSER = argparse.ArgumentParser(description=__doc__)
 
+
 def main(args=None):
     """Entry point of the script."""
     opts = PARSER.parse_args(args)
     print(opts)
+
 
 if __name__ == "__main__":
     main()
